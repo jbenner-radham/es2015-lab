3928e0141f7f17b819858524fc77b5ff2a94ee68
James Benner <james_benner@abercrombie.com>
2016-10-16 19:23:11 -0400

    Updated install script to dynamically choose between npm and yarn.

--- install-labs.sh
+++ install-labs.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
+if hash yarn 2>/dev/null; then
+	PKG_MGR="yarn"
+else
+	PKG_MGR="npm"
+fi
+
 for dir in $(ls labs); do
     if [ -f "labs/${dir}/package.json" ]; then
-        echo "Installing \"${dir}\" lab..."
-        (cd "labs/${dir}" && npm install)
+        echo "Installing \"${dir}\" lab with ${PKG_MGR}..."
+        (cd "labs/${dir}" && ${PKG_MGR} install)
     	echo "\n"
     fi
 done
